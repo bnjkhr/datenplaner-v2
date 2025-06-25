e311d54a0451028f49dcb0248da8aa66b5f5e899
Ben Kohler <benkohler@BensPubAir.fritz.box>
2025-06-25 14:56:12 +0200

    Log File

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -8,17 +8,18 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // URL zum Abonnieren des Confluence-Kalenders
-export const confluenceCalendarUrl = process.env.REACT_APP_CONFLUENCE_CALENDAR_URL;
+export const confluenceCalendarUrl =
+  process.env.REACT_APP_CONFLUENCE_CALENDAR_URL;
 export const calendarProxyUrl = process.env.REACT_APP_CALENDAR_PROXY_URL;
 
-
+export const logServerUrl = process.env.REACT_APP_LOG_SERVER_URL;
 
 // --- App ID for Firestore paths ---
-export const appId = 'datenplaner-app-v3';
+export const appId = "datenplaner-app-v3";
 
 // --- Initialize Firebase ---
 const app = initializeApp(firebaseConfig);
